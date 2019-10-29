b20a1e0778e1f48a1bdd14f722934efd79720cf8
JosueDurand96 <josue.durand8@gmail.com>
2019-10-28 22:23:47 -0500

    a

--- app/src/main/java/com/example/admin/kotlinvideorecyclerview/viewmodel/CategoryViewModel.kt
+++ app/src/main/java/com/example/admin/kotlinvideorecyclerview/viewmodel/CategoryViewModel.kt
@@ -8,13 +8,11 @@ import com.example.admin.kotlinvideorecyclerview.R
 import com.example.admin.kotlinvideorecyclerview.model.Category
 import com.squareup.picasso.Picasso
 
-class CategoryViewModel : ViewModel
-
-{
+class CategoryViewModel : ViewModel {
     var id = ""
     var title = ""
-    var desc=""
-    var imagepath=""
+    var desc = ""
+    var imagepath = ""
 
     constructor() : super()
     constructor(category: Category) : super() {
@@ -25,58 +23,52 @@ class CategoryViewModel : ViewModel
     }
 
 
-    var  arraylistmutablelivedata= MutableLiveData<ArrayList<CategoryViewModel>>()
+    var arraylistmutablelivedata = MutableLiveData<ArrayList<CategoryViewModel>>()
 
     var arraylist = ArrayList<CategoryViewModel>()
 
 
-    fun  getImageUrl():String{
+    fun getImageUrl(): String {
         return imagepath
     }
 
 
-    fun getArrayList():MutableLiveData<ArrayList<CategoryViewModel>>
-    {
+    fun getArrayList(): MutableLiveData<ArrayList<CategoryViewModel>> {
 
-         val category1 = Category("1","Josue Durand","primero","image1.png")
-         val category2 = Category("2","Josue Durand","segundo","image1.png")
+        val category1 = Category("1", "Josue Durand", "primero", "image1.png")
+        val category2 = Category("2", "Josue Durand", "segundo", "image1.png")
+        val category3 = Category("2", "Josue Durand", "segundo", "image1.png")
 
-
-        val  categoryviewmodel1 :CategoryViewModel = CategoryViewModel(category1)
-        val  categoryviewmodel2 :CategoryViewModel = CategoryViewModel(category2)
-
+        val categoryviewmodel1: CategoryViewModel = CategoryViewModel(category1)
+        val categoryviewmodel2: CategoryViewModel = CategoryViewModel(category2)
+        val categoryviewmodel3: CategoryViewModel = CategoryViewModel(category3)
 
         arraylist!!.add(categoryviewmodel1)
         arraylist!!.add(categoryviewmodel2)
-
+        arraylist!!.add(categoryviewmodel3)
         arraylistmutablelivedata.value = arraylist
 
         return arraylistmutablelivedata
     }
 
 
-
-
-
 }
 
 
- object ImageBindingAdapter{
+object ImageBindingAdapter {
 
 
-     @JvmStatic
-     @BindingAdapter("android:src")
+    @JvmStatic
+    @BindingAdapter("android:src")
 
-    fun setImageUrl(view:ImageView,url:String)
-     {
+    fun setImageUrl(view: ImageView, url: String) {
 
-         Picasso.with(view.context).load(url).placeholder(R.drawable.user).into(view)
+        Picasso.with(view.context).load(url).placeholder(R.drawable.user).into(view)
 
-     }
-
+    }
 
 
- }
+}
 
 
 
